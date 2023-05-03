9637fc5a992548267db205a5cbaf76b526e835cb
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-05-03 20:47:10 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge,uCollage) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge,collage:uCollage, id: Math.random().toString() },
       ];
     });
   };
